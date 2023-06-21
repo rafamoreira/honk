b7689b4c3847aed276912c84b54909f54c8fae29
Rafael M Cunha <me@rafaelmc.net>
2023-06-21 22:57:24 +0200

    rethink the models

--- circus/models.py
+++ circus/models.py
@@ -8,28 +8,54 @@ class CircusModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract=True
+        abstract = True
 
+
+class Clown(CircusModel):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='clown_images')
+
+    def __str__(self):
+        return self.name
 
 class Honk(CircusModel):
     honker = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='honker',
+        related_name='honks',
     )
-    clown = models.ForeignKey(
+    honked = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='clown',
+        related_name='honked',
+    )
+    clown = models.ForeignKey(
+        Clown,
+        on_delete=models.CASCADE,
+        related_name='honks',
+        null=True,
     )
     seen = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.honker} honked {self.clown}'
+        return f'{self.honker} honked {self.honked} with {self.clown}'
 
-    def get_honk(self):
-        if not self.seen:
-            self.seen = timezone.now()
-            self.save()
-        return f'{self}'
+    @classmethod
+    def get_honks(cls, honked_id):
+        honks = cls.objects.filter(
+            honked_id=honked_id,
+        ).order_by(
+            '-created_at'
+        ).select_related(
+            'clown', 'honker', 'honked'
+        )
+        honks_list = []
+        for honk in honks:
+            honk_dict = {
+                'honker': honk.honker.username,
+                'clown_name': honk.clown.name,
+                'clown_image': honk.clown.image.url,
+            }
+            honks_list.append(honk_dict)
 
+        return honks_list
