72bd6258147b83720ae9f4153325e2c8d80bc261
Rafael M Cunha <me@rafaelmc.net>
2023-06-29 23:34:29 +0200

    separate api and web views

--- circus/urls.py
+++ circus/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from circus import views
+from circus import api_views
 
 urlpatterns = [
     path("", views.home, name='home'),
     path("new_honks", views.new_honks, name='new_honks'),
     path("all_honks", views.all_honks, name='all_honks'),
-    path("honks/", views.honk_list),
-    path("honks/<int:pk>/", views.honk_detail),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("honks/", api_views.honk_list),
+    path("honks/<int:pk>/", api_views.honk_detail),
+    path("users/", api_views.UserList.as_view()),
+    path("users/<int:pk>/", api_views.UserDetail.as_view()),
 ]
