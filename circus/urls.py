90051456ad0267d24da488bd326315bf25e0beb8
Rafael M Cunha <me@rafaelmc.net>
2023-07-02 12:05:15 +0200

    rename views to web_views

--- circus/urls.py
+++ circus/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from circus import views
+from circus import web_views
 from circus import api_views
 
 urlpatterns = [
-    path("", views.home, name='home'),
-    path("new_honks", views.new_honks, name='new_honks'),
-    path("all_honks", views.all_honks, name='all_honks'),
+    path("", web_views.home, name='home'),
+    path("new_honks", web_views.new_honks, name='new_honks'),
+    path("all_honks", web_views.all_honks, name='all_honks'),
     path("honks/", api_views.honk_list),
     path("honks/<int:pk>/", api_views.honk_detail),
     path("users/", api_views.UserList.as_view()),
